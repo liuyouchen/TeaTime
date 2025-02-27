5d44066e470b848f682abc1122ac9888d86cb054
ycliu <w0909527368@gmail.com>
2025-02-27 22:29:11 +0800

    2

--- TeaTimeDemo.Models/obj/Debug/net8.0/TeaTimeDemo.Models.AssemblyInfo.cs
+++ TeaTimeDemo.Models/obj/Debug/net8.0/TeaTimeDemo.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeaTimeDemo.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dbea744af76f2368cd5a1e069154a7caf462479")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeaTimeDemo.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeaTimeDemo.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
