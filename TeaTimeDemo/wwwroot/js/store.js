000c2ab48477c93b5696a0e38037160c0e6693e7
ycliu <w0909527368@gmail.com>
2025-03-05 01:16:01 +0800

    0305-2

--- TeaTimeDemo/wwwroot/js/store.js
+++ TeaTimeDemo/wwwroot/js/store.js
@@ -22,7 +22,7 @@ function loadDataTable() {
                     <a onClick=Delete('/admin/store/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
                     </div>`
                 },
-                "width": "15%"
+                "width": "25%"
             }
         ]
     });
