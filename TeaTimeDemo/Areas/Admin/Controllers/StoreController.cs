c11684f2f653b9d82f1fb173a4cef183459f5c6e
ycliu <w0909527368@gmail.com>
2025-03-05 01:44:23 +0800

    0305-2

--- TeaTimeDemo/Areas/Admin/Controllers/StoreController.cs
+++ TeaTimeDemo/Areas/Admin/Controllers/StoreController.cs
@@ -91,7 +91,7 @@ namespace TeaTimeDemo.Areas.Admin.Controllers
             else
             {
                 Store storeObj = _unitOfWork.Store.Get(u => u.Id == id);
-                return View(storeObj);
+                return View(storeObj);//view model
             }
         }
         [HttpPost]
